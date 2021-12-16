617b086b6a53a9779b47a7d65f7d944382a5c602
nixer89 <pbnixer@gmail.com>
2021-12-16 20:54:33 +0100

    add xdx specifica

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -13,20 +13,20 @@ export enum WSSEndpoint {
 // Retry limit for reliable send
 export const RETRY_LIMIT = process.env.RETRY_LIMIT || '10'
 //file properties
-export const INPUT_CSV_FILE = process.env.INPUT_CSV_FILE || './test/input.csv';
+export const INPUT_CSV_FILE = process.env.INPUT_CSV_FILE || './input_xdx.csv';
 export const OUTPUT_CSV_FILE = process.env.OUTPUT_CSV_FILE || './test/output.csv';
 export const FAILED_TRX_FILE = process.env.FAILED_TRX_FILE || './test/failed.csv';
 export const ALREADY_SENT_ACCOUNT_FILE = process.env.ALREADY_SENT_ACCOUNT_FILE || './test/alreadyDistributedAccounts'
 
 //xrpl network
 export const XRPL_NETWORK = process.env.XRPL_NETWORK || 'mainnet';
-export const XRP_LEDGER_VERSION = process.env.XRP_LEDGER_VERSION || '';
+export const XRP_LEDGER_VERSION = process.env.XRP_LEDGER_VERSION || '68214640';
 
 //issuer properties
 //export const ISSUER_ADDRESS = process.env.ISSUER_ADDRESS || 'rHP4bHzghBdzskqcaPciL5WRGkHosB5zYx'; // <--- real MGS!
-export const ISSUER_ADDRESS = process.env.ISSUER_ADDRESS || '';
+export const ISSUER_ADDRESS = process.env.ISSUER_ADDRESS || 'rMJAXYsbNzhwp7FfYnAsYP5ty3R9XnurPo';
 
-export const CURRENCY_CODE = process.env.CURRENCY_CODE || '';
-export const TOKEN_AMOUNT = process.env.TOKEN_AMOUNT || '';
-export const DISTRIBUTOR_ACCOUNT = process.env.DISTRIBUTOR_ACCOUNT || '';
-export const DISTRIBUTOR_SECRET = process.env.DISTRIBUTOR_SECRET || '';
+export const CURRENCY_CODE = process.env.CURRENCY_CODE || 'XDX';
+export const TOKEN_AMOUNT = process.env.TOKEN_AMOUNT || '48800';
+export const DISTRIBUTOR_ACCOUNT = process.env.DISTRIBUTOR_ACCOUNT || 'rDLPDG8pNpXsk4xFxHApHd83W7nKUAvfzf';
+export const DISTRIBUTOR_SECRET = process.env.DISTRIBUTOR_SECRET || 'snQoG4wQre2GabCiMaoT8w61zWEAT';
